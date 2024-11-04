db9589c457ac427ea84e2299c5b8d35211959115
Filip Dorian Pindej <fpindej@users.noreply.github.com>
2024-11-04 23:26:12 +0100

    Create Test command

---
+++ src/Discord.Commands/Modules/TestModule.cs
@@ -0,0 +1,27 @@
+﻿using Discord.Interactions;
+using Microsoft.Extensions.Logging;
+
+namespace Discord.Commands.Modules;
+
+public class TestModule : InteractionModuleBase<SocketInteractionContext>
+{
+    private readonly ILogger<TestModule> _logger;
+
+    public TestModule(ILogger<TestModule> logger)
+    {
+        _logger = logger;
+    }
+
+    [SlashCommand("hi", "Tells you to fuck off.")]
+    public async Task TestCommand(string? hi = null)
+    {
+        await DeferAsync();
+
+        if (hi is not null)
+        {
+            await FollowupAsync("Oh hello!");
+        }
+
+        await FollowupAsync("Fuck off mate.");
+    }
+}
