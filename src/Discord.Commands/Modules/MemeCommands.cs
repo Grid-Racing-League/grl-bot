1b3494d9dce904fde24ec8030cd64a7ec0f90cf6
Filip Dorian Pindej <fpindej@users.noreply.github.com>
2025-03-23 19:42:18 +0100

    Create russian roulette

---
+++ src/Discord.Commands/Modules/MemeCommands.cs
@@ -0,0 +1,24 @@
+﻿using Discord.Interactions;
+
+namespace Discord.Commands.Modules;
+
+
+public class MemeCommands : InteractionModuleBase<SocketInteractionContext>
+{
+    [SlashCommand("roulette", "Can you survive the roulette?")]
+    public async Task Roulette()
+    {
+        await DeferAsync(ephemeral: true);
+
+        var survived = new Random().Next(1, 7) != 1;
+
+        if (survived)
+        {
+            await FollowupAsync($"{Context.User.Mention} survived the Russian roulette! *Click*", ephemeral: true);
+        }
+        else
+        {
+            await FollowupAsync($"{Context.User.Mention} didn't survive the Russian roulette! 💥", ephemeral: false);
+        }
+    }
+}
